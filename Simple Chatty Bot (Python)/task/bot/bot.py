f2ace9a1636585f8f6b90af6449bfc19a0baf9e1
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-06 14:48:18 +0300

    Stage 3/5: Guess the age

--- Simple Chatty Bot (Python)/task/bot/bot.py
+++ Simple Chatty Bot (Python)/task/bot/bot.py
@@ -1,4 +1,8 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
-print(f'What a great name you have, {input()}!')
+print(f"""Hello! My name is Aid.
+I was created in 2020.
+Please, remind me your name.
+What a great name you have, {input()}!
+Let me guess your age.
+Enter remainders of dividing your age by 3, 5 and 7.
+Your age is {(int(input()) * 70 + int(input()) * 21 + int(input()) * 15) % 105}; that's a good time to start programming!
+""")
