c7ed3cac9d22442bedb94cf3009c5d1bf6d87e52
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-06 14:43:56 +0300

    Stage 2/5: Print your name

--- Simple Chatty Bot (Python)/task/bot/bot.py
+++ Simple Chatty Bot (Python)/task/bot/bot.py
@@ -1,2 +1,4 @@
-print("""Hello! My name is Aid.
-I was created in 2020.""")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+print(f'What a great name you have, {input()}!')
